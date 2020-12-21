1af557c43d80bcc754071a35af4fc65619a04253
Avantika Sarkar <avantikasarkar18@gmail.com>
2020-12-21 19:52:27 +0530

    Updated Functionality

--- sample_website/src/App.js
+++ sample_website/src/App.js
@@ -2,9 +2,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
+    <>
+   <Router>
+     <Navbar />
+     <Switch>
+        <Route path='/' exact />
+     </Switch>
+   </Router>
+        
+  </>
    
   );
 }
