24eaaa0fb00ffbd1f64954ad293a3cb46d024fda
Avantika Sarkar <avantikasarkar18@gmail.com>
2020-12-25 17:13:47 +0530

    Meme Generator - Phase 1

--- meme_generator/src/Header.js
+++ meme_generator/src/Header.js
@@ -9,6 +9,7 @@ function Header()
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
                 alt= "Problem ?"
             />
+            <p><b>Meme Generator</b></p>
         </header>
     )
 }
--- meme_generator/src/MemeGenerator.js
+++ meme_generator/src/MemeGenerator.js
@@ -12,21 +12,50 @@ class MemeGenerator  extends Component{
             randomImg : "http://i.imgflip.com/1bij.jpg",
             allImages : []
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
 
         const url = "https://api.imgflip.com/get_memes"
         fetch(url)
+
+        //copying the JSON file data in response variable
             .then(response=> response.json())
+
+        
             .then (response =>{
+                    //getting the array of images
                         const {memes} = response.data
-                        console.log(memes[0])
+                       
+                    //since we cannot setState directly, setting the state of 'allImages' to memes
                         this.setState({allImages : memes})
                     }
                 )
     }
 
+
+    handleChange(event){
+
+        //setting name = event.target.name & value= event.target.value
+        const {name, value} = event.target
+
+        //place the value of input received in the input field with the name...
+        this.setState({[name] : value})
+    }
+
+    handleSubmit(event){
+
+        //to not submit right away
+        event.preventDefault()
+
+        //getting a random index from allImages array
+        const randNum = Math.floor(Math.random() * this.state.allImages.length)
+        //get the url from that index
+        const randNumMeme = this.state.allImages[randNum].url
+        this.setState({randomImg : randNumMeme})
+    }
     
     
 
@@ -34,13 +63,13 @@ class MemeGenerator  extends Component{
 
         return (
             <div>
-                <form className = "meme-inputs">
+                <form className = "meme-form" onSubmit={this.handleSubmit}>
                     <input 
                     type='text' 
                     name ="topText" 
                     placeholder ="Enter first text..."
                     value = {this.state.topText}
-                    //onChange = {}
+                    onChange = {this.handleChange}
                      />
 
                     <br/>
@@ -50,12 +79,18 @@ class MemeGenerator  extends Component{
                     name ="bottomText" 
                     placeholder ="Enter second text here..."
                     value = {this.state.bottomText}
-                    //onChange ={}
+                    onChange ={this.handleChange}
                     />
                     <br/>
 
                     <button >Generate </button>
                 </form>
+
+                <div className='meme'>
+                    <img src={this.state.randomImg}  alt=""/>
+                    <h2 className ="top"> {this.state.topText} </h2>
+                    <h2 className="bottom"> {this.state.bottomText} </h2>
+                </div>
             </div>
         )
     }
